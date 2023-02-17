0e71ab3ecd78b72c35965efff4efc77480ba715e
Zeeb <94621584+Zeeb1988@users.noreply.github.com>
2023-02-17 15:45:13 +0800

    Update

--- UnityCsharp.cs
+++ UnityCsharp.cs
@@ -156,3 +156,33 @@ public class EnableComponents : MonoBehaviour
         }
     }
 }
+
+7.Translate和Rotate
+Vector3(0,0,1) 快捷写法 Vector3.forword
+
+Vector3(0,1,0) 快捷写法 Vector3.up
+
+using UnityEngine;
+using System.Collections;
+
+public class TransformFunctions : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    
+    
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
